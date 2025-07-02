f4516e91e805f305bdcb1655cf21447e4c7d98cd
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2025-07-02 21:51:34 +0530

    feat: update main app layout and navigation structure

--- app/index.jsx
+++ app/index.jsx
@@ -49,7 +49,8 @@ const Welcome = () => {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => router.push("/sign-in")}
+            // handlePress={() => router.push("/sign-in")}
+                handlePress={() => router.push("/home")}
             containerStyles="w-full mt-7"
           />
         </View>
