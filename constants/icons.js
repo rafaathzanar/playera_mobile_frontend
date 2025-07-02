47e8485209d8c44b3b0f82a67e3c4d69552ea795
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2025-07-02 21:51:24 +0530

    feat: add Colors constants and update theme hooks

--- constants/icons.js
+++ constants/icons.js
@@ -2,7 +2,7 @@ import bookmark from "../assets/icons/bookmark.png";
 import home from "../assets/icons/home.png";
 import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
-import calendar from "../assets/icons/calendar.png";
+import calendar from "../assets/icons/calendar.png"; // Add this line
 import menu from "../assets/icons/menu.png";
 import search from "../assets/icons/search.png";
 import upload from "../assets/icons/upload.png";
@@ -12,7 +12,7 @@ import eye from "../assets/icons/eye.png";
 import notification from "../assets/icons/notification.png";
 
 export default {
-  calendar,
+  calendar, // Add this line
   home,
   plus,
   profile,
